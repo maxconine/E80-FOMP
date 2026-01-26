ccde7b389390f25c7fdd197d99583cbdd74741d5
maxconine <58146795+maxconine@users.noreply.github.com>
2026-01-26 09:46:52 -0800

    Working on Lab 1

--- MATLAB/debugme.m
+++ MATLAB/debugme.m
@@ -16,30 +16,30 @@ NumCols = 10;                     % desired width of the output matrix
 A = zeros(NumCols*3+2,NumCols);   % matrix of all zeros with the desired size
 
 % Step 1: Set the first and last rows of the matrix appropriately
-A(1,:)=ones(1,numcols);
-A(numcols*3+2,:)=ones(1,numcols);
+A(1,:)=ones(1,NumCols);
+A(end,:)=ones(1,NumCols);
 
 % Step 2: Set up the first block of the matrix
 % We use a for loop to iterate through the rows of this block.
 % The command "for row=1:5" would execute the body of the loop
 % with row=1, then row=2, and so on, until row=5. The "end" command
 % designates the end of the section of code that gets repeated by the loop.
-for row=2:(2+numcols)
+for row=2:(NumCols+1)
     A(row,row-1)=1;
 end
 
 % Step 3: Set up the middle block of the matrix
-for row=(3+numcols):(3+2*numcols)
+for row=(2+NumCols):(1+2*NumCols)
     if mod(row,2)==0  % If you're not sure what mod does, look it up!
-        A(row,row-numcols-2)=5;
+        A(row,row-NumCols-1)=5;
     else
-        A(row,row-numcols-2)=-5;
+        A(row,row-NumCols-1)=-5;
     end
 end
 
 % Step 4: Set up the final block of the matrix
 col=1;
-for row=(4+2*numcols):(3*numcols+2)
+for row=(22):(31)
     A(row,col)=col;
     col=col+1;
 end
